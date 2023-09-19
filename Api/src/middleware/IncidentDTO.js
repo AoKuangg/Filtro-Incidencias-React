2a4169fe7a66a29612620b00ae4dbd2e9de7379d
AoKuangg <camipa2305@gmail.com>
2023-09-19 13:17:50 -0500

    feat: :sparkles: Dto added to the proyect

---
+++ Api/src/middleware/IncidentDTO.js
@@ -0,0 +1,12 @@
+import { check } from "express-validator";
+
+export const incidentSchema = [
+  check("Site").isString().notEmpty().withMessage("Site is required"),
+  check("Tittle").isString().notEmpty().withMessage("Tittle is required"),
+  check("Description").isString().notEmpty().withMessage("Description is required"),
+  check("Severity").isString(),
+  check("Category").isString(),
+  check("DateOfIncident").isString().notEmpty().withMessage("Date of incident is required"),
+  check("Diagnosis").isString(),
+  check("Status").isString(),
+]
--- Api/src/version/v1/routes/camper.routes.js
+++ Api/src/version/v1/routes/camper.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as CamperConsulta from "../../../controller/camperController.js"
 import { validationSchema } from "../../../helpers/validatorschema.js";
+import { incidentSchema } from "../../../middleware/IncidentDTO.js";
 
 const Camper = Router();
 
-Camper.use("/NewReport",CamperConsulta.CreateIncident);
+Camper.use("/NewReport",validationSchema(incidentSchema),CamperConsulta.CreateIncident);
 
 export default Camper;
