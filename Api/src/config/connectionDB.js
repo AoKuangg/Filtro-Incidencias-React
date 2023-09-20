0096e79b967435e713d3760cc274d97380c9d9bb
AoKuangg <camipa2305@gmail.com>
2023-09-20 07:59:57 -0500

    feat: :sparkles: Added the corrected url for the database

--- Api/src/config/connectionDB.js
+++ Api/src/config/connectionDB.js
@@ -5,7 +5,7 @@ const DB = JSON.parse(process.env.ATLAS_CONNECTION);
 
 export async function ConnectDb(){
   try {
-    const URL = `mongodb+srv://${DB.user}:${DB.password}@cluster0.j4ctnar.mongodb.net/`
+    const URL = `mongodb+srv://${DB.user}:${DB.password}@cluster0.j4ctnar.mongodb.net/${DB.database}`
     const client = await MongoClient.connect(URL);
     return client.db();
   } catch (error) {
