6f54ae3ae06cc5ac82ed740966570c56e84fb4f1
AoKuangg <camipa2305@gmail.com>
2023-09-22 07:12:52 -0500

    feat: :sparkles: Added the Support routes to the index file

--- Api/src/version/v1/index.js
+++ Api/src/version/v1/index.js
@@ -3,6 +3,7 @@ import { limitRequest } from "../../config/limit.js";
 import Camper from "./routes/camper.routes.js";
 import loginApp from "./routes/login.routes.js";
 import Trainer from "./routes/trainer.routes.js";
+import Support from "./routes/support.routes.js";
 
 const v1Routers = Router();
 v1Routers.use(limitRequest());
@@ -10,5 +11,6 @@ v1Routers.use(limitRequest());
 v1Routers.use("/auth",loginApp);
 v1Routers.use("/CamperView",Camper);
 v1Routers.use("/TrainerView",Trainer);
+v1Routers.use("/SupportView",Support)
 
 export default v1Routers;
