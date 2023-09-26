a169db1e07543b375ff63b9f08bd5ffe0a68e5a5
AoKuangg <camipa2305@gmail.com>
2023-09-26 09:26:20 -0500

    style: :lipstick: Updated the style of the componen navbar and landingpage, also added a img to help the landingpage component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between px-4 py-2 bg-white border-b border-gray-200'>
-      <Link to='/'>
-        <h1 className='text-xl font-bold'>Campuslands Incidents</h1>
+    <nav className="flex items-center justify-between px-4 py-2 bg-slate-700 border-b border-gray-200">
+      <Link to="/">
+        <h1 className="text-xl font-bold text-teal-400">
+          Campuslands Incidents
+        </h1>
       </Link>
-      <div className='flex items-center gap-2'>
-        <Link to='/SignIn'>
-          <button className='px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md'>Login</button>
+      <div className="flex items-center gap-2">
+        <Link to="/SignIn">
+          <button className="px-4 py-2 bg-emerald-400 hover:bg-emerald-600 text-white rounded-md">
+            Login
+          </button>
         </Link>
-        <Link to='/signUp'>
-          <button className='px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded-md'>Sign Up</button>
+        <Link to="/signUp">
+          <button className="px-4 py-2 bg-emerald-400 hover:bg-emerald-600 text-white rounded-md">
+            Sign Up
+          </button>
         </Link>
       </div>
     </nav>
--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import Navbar from '../Navbar';
-
+import React from "react";
+import Navbar from "../Navbar";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
-      <h1>Campuslands Incidents</h1>
+      <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden ">
+        <div className="w-full lg:flex lg:justify-center lg:items-center p-10 ">
+          <img
+            src="/assets/incident.png"
+            className="max-w-full h-auto md:w-2/3 lg:w-1/3 sm:w-full md:mx-auto lg:mx-0"
+            alt=""
+          />
+          <div className="text-center md:text-center  mt-4 md:mt-0 md:ml-4">
+            <h1 className="text-4xl md:text-4xl lg:text-5xl">
+              Welcome to the Campuslands Incident Management
+            </h1>
+            <p className="mt-4 text-xl md:text-xl lg:text-2xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse iaculis magna eget mi tincidunt, eget congue est
+              scelerisque.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
