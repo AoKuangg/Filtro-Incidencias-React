e60149fdc8daef27426779651021f5e6d6a63caf
AoKuangg <camipa2305@gmail.com>
2023-09-28 08:27:09 -0500

    refactor: :truck: Rename TrainerView.jsx

---
+++ src/components/pages/TrainerView.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const SERVER = JSON.parse(import.meta.env.VITE_SERVER);
+export default function TrainerVirew() {
+  
+
+  return <div>TrainerVirew</div>;
+}
