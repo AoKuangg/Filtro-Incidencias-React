91b41c99b988b2fd4dabad567224a31176694639
AoKuangg <camipa2305@gmail.com>
2023-10-03 07:01:13 -0500

    feat: :sparkles: Updated limit.js in order to make mor petitions

--- Api/src/config/limit.js
+++ Api/src/config/limit.js
@@ -2,8 +2,8 @@ import rateLimit from "express-rate-limit";
 
 export const limitRequest = () => {
   return rateLimit({
-    windowMs: 30 * 1000, // 15 minutes
-    max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: (req, res) => {
